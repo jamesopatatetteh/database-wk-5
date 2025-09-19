e6015cf307918ca0ab2f4ed26109669cdd520683
James <jamesopata220@gmail.com>
2025-09-19 21:03:51 +0000

    Updated commit

---
+++ answers.sql
@@ -0,0 +1,14 @@
+-- Question 1 🗑️
+DROP INDEX IdxPhone ON customers;
+
+-- Question 2 👤
+CREATE USER 'bob'@'localhost' IDENTIFIED BY 'S$cu3r3!';
+
+-- Question 3 🔑
+GRANT INSERT ON salesDB.* TO 'bob'@'localhost';
+
+-- Question 4 🔐
+ALTER USER 'bob'@'localhost' IDENTIFIED BY 'P$55!23';
+
+-- Refresh privileges
+FLUSH PRIVILEGES;
